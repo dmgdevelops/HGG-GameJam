c4879b06bf4822cb30c8563f36d9acd69f220438
Hakasay <hakasayn64@gmail.com>
2025-09-30 17:55:16 -0500

    Forest Music Added
    Added AudioStream to all forest scenes.
    Added Boss Room Overlay/Boundaries/BossMusic

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,3 +3,15 @@ extends Node
 var morgan_page_collection = 0
 var morgan_picture_collection = 0
 var morgan_enemies_slayed = 0
+
+#scripted loss
+var initialEncounterWithShadowMorgan = false
+
+#after puzzle room 1 (only update if defeated Shadow Morgan)
+var secondEncounterWithShadowMorgan = false
+
+#after puzzle room 2 (only update if defeated Shadow Morgan)
+var thirdEncounterWithShadowMorgan = false
+
+#after puzzle room 3 (only update if defeated Shadow Morgan)
+var finalEncounterWithShadowMorgan = false

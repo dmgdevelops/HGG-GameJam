b4a604c650c28cf01467414d668b61daf3cadac3
David Garcia <56805040+dmgdevelops@users.noreply.github.com>
2025-09-30 19:53:21 -0400

    First Dialogue

--- Scripts/StageScripts/DreamWorldWakeUp.gd
+++ Scripts/StageScripts/DreamWorldWakeUp.gd
@@ -5,3 +5,12 @@ extends Node2D
 func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent( self )
+	if Global.initialEncounterWithShadowMorgan == false:
+		PlayerManager.player.set_physics_process(false)
+		Dialogic.timeline_ended.connect(ended)
+		Dialogic.start("Forest")
+
+func ended():
+	Dialogic.timeline_ended.disconnect(ended)
+	PlayerManager.player.set_physics_process(true)
+	print("ended forest timeline")

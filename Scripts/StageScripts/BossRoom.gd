e6706e378c82cad3f6239f76b5c4ce2172ab5775
David Garcia <56805040+dmgdevelops@users.noreply.github.com>
2025-09-30 22:00:26 -0400

    Connecting Levels

--- Scripts/StageScripts/BossRoom.gd
+++ Scripts/StageScripts/BossRoom.gd
@@ -4,11 +4,14 @@ extends Node2D
 @onready var doorOpen = $SmallDoor/OpenDoor
 @onready var doorBarrier = $SmallDoor/DoorCollisions/DoorBarrier
 @onready var boss: Boss = $DarkMorgan_boss
+@onready var player_spawn: Node2D = $PlayerSpawn
 
 
 func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent( self )
+	PlayerManager.set_player_pos(player_spawn.global_position)
+	LevelManager.level_load_started.connect ( _free_level )
 	if Global.initialEncounterWithShadowMorgan == false:
 		PlayerManager.player.set_physics_process(false)
 		boss.state_machine.set_process(false)
@@ -25,3 +28,7 @@ func ended():
 func _on_dialogic_signal(arg : String):
 	if arg == "sword":
 		Global.initialEncounterWithShadowMorgan = true
+
+func _free_level() -> void:
+	queue_free()
+	PlayerManager.unparent_player(self)
--- Scripts/StageScripts/PuzzleRoom3Manager.gd
+++ Scripts/StageScripts/PuzzleRoom3Manager.gd
@@ -3,11 +3,14 @@ extends Node2D
 @onready var doorClosed = $SmallDoor/ClosedDoor
 @onready var doorOpen = $SmallDoor/OpenDoor
 @onready var doorBarrier = $SmallDoor/DoorCollisions/DoorBarrier
+@onready var player_spawn: Node2D = $PlayerSpawn
+
 var numEnemiesSlayed = 3
 
 func _ready():
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent( self )
+	PlayerManager.set_player_pos(player_spawn.global_position)
 	pass
 
 func _process(_delta):
@@ -25,3 +28,7 @@ func _on_reset_button_reset_pressed():
 	for box in boxes2:
 			box.get_child(0).reset_position()
 	pass # Replace with function body.
+
+func _free_level() -> void:
+	queue_free()
+	PlayerManager.unparent_player(self)

addb37b4e040211ad65c997a84679dfc7ef7897a
Hakasay <hakasayn64@gmail.com>
2025-09-30 19:33:22 -0500

    Doors
    Fixed some door stuff

--- Scripts/StageScripts/DreamWorldWakeUp.gd
+++ Scripts/StageScripts/DreamWorldWakeUp.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
+@onready var middleDoorOpen = $BigDoor/BigDoorOpen
+@onready var middleDoorClosed = $BigDoor/BigDoorClosed
+@onready var middleDoorBarrier = $BigDoor/DoorCollisions/DoorBarrier
 
+@onready var leftDoorOpen = $DoorLeft/ClosedDoor
+@onready var leftDoorClosed = $DoorLeft/OpenDoor
+@onready var leftDoorBarrier = $DoorLeft/DoorCollisions/DoorBarrier
+
+@onready var rightDoorOpen = $DoorRight/ClosedDoor
+@onready var rightDoorClosed = $DoorRight/OpenDoor
+@onready var rightDoorBarrier = $DoorRight/DoorCollisions/DoorBarrier
 
 func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent( self )
+
+func _process(_delta):
+	pass

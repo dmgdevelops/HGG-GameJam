c4a526a9bc5632dbfbc9dddad4db1a62b54afe79
David Garcia <56805040+dmgdevelops@users.noreply.github.com>
2025-09-30 21:10:18 -0400

    Scene Transition

--- Scripts/StageScripts/DreamWorldWakeUp.gd
+++ Scripts/StageScripts/DreamWorldWakeUp.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 
 func _ready() -> void:
+	await SceneTransition.fade_in()
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent( self )
 	if Global.initialEncounterWithShadowMorgan == false:
